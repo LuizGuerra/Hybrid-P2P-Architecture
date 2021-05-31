337a0c231168f65949049c8c3a662fac72b3aed8
LuizGuerra <luizpedrofg@gmail.com>
2021-05-31 01:58:05 -0300

    File transfer

---
+++ src/ReceiveFile.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ReceiveFile implements Runnable, Closeable {
+
+    static String thisPath = System.getProperty("user.dir");
+
+    int port;
+
+    ServerSocket serverSocket;
+
+    public ReceiveFile(String ip, int port) throws IOException {
+        this.port = port;
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.serverSocket.close();
+    }
+
+    @Override
+    public void run() {
+        byte[] bytes;
+        // input socket data
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                InputStream inputStream = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String fileName = reader.readLine();
+                System.out.println("Receiving data...");
+                System.out.println("File name: " + fileName);
+
+                byte[] buffer = new byte[1024*8];
+                int bytesRead;
+
+                File newFile = new File(thisPath + "/"+ fileName);
+                OutputStream outputStream = new FileOutputStream(newFile);
+
+                while ( (bytesRead = inputStream.read(buffer)) != -1 ) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                System.out.println("Successfully created file");
+
+                System.out.println();
+                socket.close();
+                inputStream.close();
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Meg used="+(Runtime.getRuntime().totalMemory()-
+                        Runtime.getRuntime().freeMemory())/(1000*1000)+"M");
+                System.gc();
+                System.out.println("Meg used="+(Runtime.getRuntime().totalMemory()-
+                        Runtime.getRuntime().freeMemory())/(1000*1000)+"M");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // necessary info
+        String ip = "127.0.0.1";
+        int port = 1234;
+        ReceiveFile receiveFile = new ReceiveFile(ip, port);
+        receiveFile.run();
+    }
+}
---
+++ src/SendFile.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SendFile implements Runnable, Closeable {
+
+    String ip;
+    InetAddress address;
+    int port;
+
+    File file;
+    BufferedReader reader;
+
+    public SendFile(String ip, int port, String filePath) throws IOException {
+        this.ip = ip;
+        this.address = InetAddress.getByName(ip);
+        this.port = port;
+        this.file = new File(filePath);
+        this.reader = new BufferedReader(new FileReader(file));
+    }
+
+    @Override
+    public void run() {
+        String line = "";
+        try {
+            // output socket data
+            Socket socket = new Socket(ip, port);
+            OutputStream outputStream = socket.getOutputStream();
+            // send file name
+            outputStream.write((file.getName() + "\n").getBytes());
+            outputStream.flush();
+            // send file bytes
+            FileInputStream fileInputStream = new FileInputStream(file);
+            outputStream.write(fileInputStream.readAllBytes());
+            outputStream.flush();
+
+            outputStream.close();
+            fileInputStream.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Meg used="+(Runtime.getRuntime().totalMemory()-
+                    Runtime.getRuntime().freeMemory())/(1000*1000)+"M");
+            System.gc();
+            System.out.println("Meg used="+(Runtime.getRuntime().totalMemory()-
+                    Runtime.getRuntime().freeMemory())/(1000*1000)+"M");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        reader.close();
+    }
+
+    // For testing purposes
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // necessary info
+        String ip = "127.0.0.1";
+        int port = 1234;
+        String rtfFile = "/Users/luizpedrofranciscattoguerra/Desktop/planning.rtf";
+        String imageFile = "/Users/luizpedrofranciscattoguerra/Desktop/TestImage.png";
+
+        SendFile sendFile = new SendFile(ip, port, imageFile);
+        sendFile.run();
+    }
+}
